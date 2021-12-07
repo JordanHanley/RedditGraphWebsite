bece12b2347bc504a38a7ccf8dcd58c65dbe4c43
Jordan Hanley <jordanhanley79@gmail.com>
2021-12-07 14:52:17 -0500

    Initial Upload

---
+++ backend/include/RedditGraph.h
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#include <utility>
+#include <iostream> 
+#include <queue>
+#include <set>
+using namespace std;
+
+class RedditGraph {
+private:
+    // Internal node subclass
+    struct Node {
+        Node* parent;
+        string value;
+        Node(Node* parent, string value) {
+            this->parent = parent;
+            this->value = value;
+        }
+    };
+    // Fields
+    unordered_map<string, vector<pair<string, int>>> data;
+    vector<string> indexToKey;
+    unordered_map<string, int> keyToIndex;
+    // Constant to be used in the djisktras algo to make the higher weight edges count as lower weight
+    // This constant must exceed the value of the highest weight edge
+    const int DIJKSTRASCONSTANT = 100000;
+    // Private Functions
+    Node* BFSHelper(string from, string to, vector<Node*>& allocatedNodes, unordered_set<string>& visited);
+public:
+    // Public Funtions
+    void readFromFile(string fileName);
+    vector<string> getBFSPathFromTo(string from, string to);
+    vector<string> getDijkstrasPathFromTo(string from, string to);
+};
